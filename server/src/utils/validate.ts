a0d96f15045d721cfe755982dc0a60d848c08d2d
Tim Fan <tfan478@gmail.com>
2021-10-06 21:16:59 +1100

    login+register now return {success,msg}.

--- server/src/utils/validate.ts
+++ server/src/utils/validate.ts
@@ -3,34 +3,34 @@ import { UsernamePassword } from "../entities/entities";
 export const validateRegister = (options: UsernamePassword) => {
     // TODO: email must be a "valid" email; it must pass a simple email regex test.
     if (!options.email.includes("@")) {
-        return [
-            {
-                name: "email",
-                msg: "invalid email",
-            },
-        ];
+        return {
+            success: false,
+            msg: "invalid email",
+        };
     }
 
     // TODO: username can't contain @. this check is important.
+    if (options.username.length <= 3) {
+        return {
+            success: false,
+            msg: "username length must be greater than x",
+        };
+    }
 
     // TODO: username must be in range [4,X], and contain 2 special symbols.
     if (options.username.length <= 3) {
-        return [
-            {
-                name: "username",
-                msg: "username length must be greater than x",
-            },
-        ];
+        return {
+            success: false,
+            msg: "username length must be greater than x",
+        };
     }
 
     // TODO: password must be good.
     if (options.password.length <= 3) {
-        return [
-            {
-                name: "password",
-                msg: "length must be greater than x",
-            },
-        ];
+        return {
+            success: false,
+            msg: "Bad password",
+        };
     }
 
     return null;
