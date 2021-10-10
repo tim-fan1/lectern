20036e7491c5da48c1c2aaded1b954102fd9fea2
Ivan-Velickovic <10481259+Ivan-Velickovic@users.noreply.github.com>
2021-10-10 20:48:17 +1100

    WIP: [front] more error handling for login/register/join and cleaning

--- client/pages/join/[code].tsx
+++ client/pages/join/[code].tsx
@@ -1,51 +1,64 @@
 import { useRouter } from "next/router";
+import { validateSessionCode } from "../../util";
 import Navigation from "../../components/Navigation";
-import styles from "../../styles/JoinCode.module.css";
+import styles from "../../styles/joincode.module.css";
 
-export default function JoinWithCode() {
-    /* TODO: Check that code is valid since it simply comes from the user defined route. */
+export default function JoinCode() {
     const router = useRouter();
     const { code } = router.query;
+
+    let isValidCode = true;
+    if (code === undefined || typeof code != "string" || !validateSessionCode(code)) {
+        isValidCode = false;
+    }
 
     return (
         <div className="container_center">
             <Navigation />
-            <h2 id={styles.header_enter_session}>
-                About to enter session <span id={styles.code}>{code}</span>
-            </h2>
-            <h1 id={styles.header_session_title}>Example session title.</h1>
-            <div>
+            {isValidCode && (
                 <div>
-                    <h3>Instructor name</h3>
-                    <p>This is the bio of the instructor.</p>
-                </div>
-            </div>
+                    <h2 id={styles.header_enter_session}>
+                        About to enter session <span id={styles.code}>{code}</span>
+                    </h2>
+                    <h1 id={styles.header_session_title}>Example session title.</h1>
+                    <div>
+                        <div>
+                            <h3>Instructor name</h3>
+                            <p>This is the bio of the instructor.</p>
+                        </div>
+                    </div>
 
-            <form className="container_center" id={styles.form_join}>
-                <div className="container_input_label">
-                    <label className="label">
-                        Enter your name to be displayed (optional)
-                    </label>
-                    <input className="input" type="text" />
+                    <form className="container_center" id={styles.form_join}>
+                        <div className="container_input_label">
+                            <label className="label">
+                                Enter your name to be displayed (optional)
+                            </label>
+                            <input className="input" type="text" />
+                        </div>
+                        <button className="btn btn_primary" id={styles.btn_continue} type="submit">
+                            Continue
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                height="24px"
+                                viewBox="0 0 24 24"
+                                width="24px"
+                                fill="#000000"
+                            >
+                                <path d="M0 0h24v24H0V0z" fill="none" />
+                                <path d="M10.02 6L8.61 7.41 13.19 12l-4.58 4.59L10.02 18l6-6-6-6z" />
+                            </svg>
+                        </button>
+                    </form>
                 </div>
-                <button
-                    className="btn btn_primary"
-                    id={styles.btn_continue}
-                    type="submit"
-                >
-                    Continue
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24px"
-                        viewBox="0 0 24 24"
-                        width="24px"
-                        fill="#000000"
-                    >
-                        <path d="M0 0h24v24H0V0z" fill="none" />
-                        <path d="M10.02 6L8.61 7.41 13.19 12l-4.58 4.59L10.02 18l6-6-6-6z" />
-                    </svg>
-                </button>
-            </form>
+            )}
+            {!isValidCode && (
+                <div id={styles.container_invalid_code}>
+                    <h2>
+                        The session code <b>#{code}</b> entered is invalid.
+                    </h2>
+                    <h2>Please double check it is the correct code.</h2>
+                </div>
+            )}
         </div>
     );
 }
