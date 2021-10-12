1b3a706ae153ef74a1d97cbbaca6c785c3b1c982
Tim Fan <tfan478@gmail.com>
2021-10-12 21:25:23 +1100

    Revert "sendgrid setup. please store api key in config.private.ts"
    This reverts commit 601896f0afe91479c0bb41b4ca3c1f31d99c2c80.

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,10 +3,7 @@
 import { config } from "dotenv";
 config();
 
-import { sendgrid_api_key } from "./config.private";
-
 export default {
     isProduction: process.env.NODE_ENV === "production",
     frontend_url: process.env.FRONTEND_URL || "http://localhost:3000",
-    sendgrid_api_key: sendgrid_api_key,
 };
