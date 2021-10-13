79e340c6b3e6a145741ce61f127f14b33aa44b0d
Jack J <jackywathy24@gmail.com>
2021-10-13 11:18:34 +1100

    add favicon icons and html tags

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import React from "react";
 import { createClient, Provider } from "urql";
 import { ContextAuthProvider } from "../contexts/ContextAuth";
+import Head from "next/head";
 import "../styles/globals.css";
 /* TODO: don't really like this solution since we're importing this all the time even if we don't use it but it
 doesn't make sense to use component level CSS modules. Maybe if we make a Form component then it would make sense. */
@@ -18,11 +19,22 @@ function App({ Component, pageProps }: AppProps) {
     });
 
     return (
-        <Provider value={client}>
-            <ContextAuthProvider>
-                <Component {...pageProps} />
-            </ContextAuthProvider>
-        </Provider>
+        <>
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <title>uwu</title>
+            </Head>
+
+            <Provider value={client}>
+                <ContextAuthProvider>
+                    <Component {...pageProps} />
+                </ContextAuthProvider>
+            </Provider>
+        </>
     );
 }
 export default App;
