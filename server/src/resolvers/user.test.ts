9c30ab93b021fc491041c926643f5c4019ccdae5
Jack <jackywathy24@gmail.com>
2021-10-30 21:16:14 +1100

    fix helpers mock

--- server/src/resolvers/user.test.ts
+++ server/src/resolvers/user.test.ts
@@ -1,67 +1,14 @@
-import supertest from "supertest";
-import generateAlphanumCode from "../utils/generateCode";
 import {
+    checkUserResponse,
+    createUser,
+    LoginMutation,
+    mockGenerateAlphanumCode,
+    RegisterMutation,
+    registerUser,
     resetDatabase,
     sendGraphqlRequest,
-    testGetAppSingleton,
+    VerifyEmailMutation,
 } from "./test/helpers";
-
-// mock the generate code first, before registering
-// this needs to be done in top level scope (as jest will actually hoist
-// the mock above the import above
-jest.mock("../utils/generateCode");
-// some funky type casting to allow ts to understand jest mock
-// https://stackoverflow.com/questions/48759035/mock-dependency-in-jest-with-typescript
-const mockGenerateAlphanumCode = generateAlphanumCode as jest.MockedFunction<
-    typeof generateAlphanumCode
->;
-// now, generateAlphanumCode will be mocked with jest's default implementation
-// which is () => return undefined. Lets make it use our original implementation
-// we can override this in a test later
-mockGenerateAlphanumCode.mockImplementation(
-    jest.requireActual("../utils/generateCode")
-        .default as typeof generateAlphanumCode
-);
-
-const RegisterMutation = `
-mutation($password: String!, $fname: String!, $lname: String!, $email: String!) {
-  register(
-    password: $password,
-    fname: $fname,
-    lname: $lname,
-    email: $email
-  ) {
-    errors {
-        msg,
-        kind
-    },
-    user {
-      email
-      id
-      name
-    }
-  }
-}`;
-
-const VerifyEmailMutation = `
-mutation($verification_code: String!) {
-    verify_email(verification_code: $verification_code) {
-        errors {
-            kind,
-            msg
-        }
-    }
-}`;
-
-const LoginMutation = `
-mutation($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-        errors {    
-            kind,
-            msg
-        }
-    }
-}`;
 
 afterEach(async () => {
     await resetDatabase();
@@ -70,42 +17,6 @@ afterEach(async () => {
 describe("graphql user detail tests", () => {
     // This function checks if a user response contains a valid
     // representation of a user from the /register register
-    const checkUserResponse = (
-        user_obj: any,
-        { email, fname, lname }: { email: string; fname: string; lname: string }
-    ) => {
-        expect(user_obj).toEqual(
-            expect.objectContaining({
-                errors: [],
-                user: {
-                    email: email,
-                    name: `${fname} ${lname}`,
-                    id: expect.any(Number),
-                },
-            })
-        );
-    };
-
-    /// Register a user, and do some basic tests to ensure state is correct
-    // after the authentication patch, application errors previously returned
-    // top level will be returned further in the object (.data.register.errors)
-    // we can just return the data body then as we no longer have to worry
-    // about the funny error conditions on the top level
-    const registerUser = async (
-        fname: string,
-        lname: string,
-        email: string,
-        password: string
-    ): Promise<any> => {
-        const res = await sendGraphqlRequest(RegisterMutation, {
-            fname,
-            lname,
-            email,
-            password,
-        });
-
-        return res.body.data.register;
-    };
 
     test("register test", async () => {
         const fname = "owo";
@@ -189,5 +100,14 @@ describe("graphql user detail tests", () => {
                 msg: "Incorrect password",
             }),
         ]);
+    });
+
+    test("login test", async () => {
+        const email = "owo@uwu.com";
+        const password = "password";
+        const user = await createUser(email, "john", "smith", password);
+        // try to login again
+        let res = await sendGraphqlRequest(LoginMutation, { email, password });
+        expect(res.body.data.login.errors).toHaveLength(0);
     });
 });
