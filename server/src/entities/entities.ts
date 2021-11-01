55470902b6a4c29b3ab998e0271601d260482b4e
Tim Fan <52772690+tf478@users.noreply.github.com>
2021-11-01 17:27:55 +1100

    Revert "server/activity - create polls"

--- server/src/entities/entities.ts
+++ server/src/entities/entities.ts
@@ -3,6 +3,5 @@ import LoginSession from "./LoginSession";
 import Session from "./Session";
 import Activity from "./Activity";
 import VerifyEmail from "./VerifyEmail";
-import Choice from "./Choice";
 
-export { User, LoginSession, Session, Activity, VerifyEmail, Choice };
+export { User, LoginSession, Session, Activity, VerifyEmail };
--- server/src/resolvers/resolvers.ts
+++ server/src/resolvers/resolvers.ts
@@ -3,6 +3,5 @@
 import HelloResolver from "./hello";
 import SessionResolver from "./session";
 import UserResolver from "./user";
-import ActivityResolver from "./activity";
 
-export { HelloResolver, UserResolver, SessionResolver, ActivityResolver };
+export { HelloResolver, UserResolver, SessionResolver };
