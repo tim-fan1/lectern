9e8da18b4ed9de841d8d1fcb00753f307e941558
Jack J <jackywathy24@gmail.com>
2021-11-05 23:35:25 +1100

    PollResult: add split and center items

--- client/components/PollResult.tsx
+++ client/components/PollResult.tsx
@@ -11,9 +11,8 @@ interface PollResultProps {
 export const PollResult = ({ title, results }: PollResultProps) => {
     let totalVotes = results.reduce((a, b) => a + b.numberOfVotes, 0);
     let highestVote = results.reduce((a, b) => Math.max(a, b.numberOfVotes), 0);
-
     return (
-        <div className={styles.main_container}>
+        <div className={`${styles.main_container} container_center`}>
             <div className={styles.top_header_container}>
                 <div className={styles.header_q_container}>
                     <h2 className={styles.top_header_q}>Q:</h2>
@@ -22,26 +21,35 @@ export const PollResult = ({ title, results }: PollResultProps) => {
                     <b>{title}</b>
                 </h3>
             </div>
-
-            {results.map((result, i) => {
-                let shownBarStyle = {
-                    width: (result.numberOfVotes / totalVotes) * 100 + "%",
-                    backgroundColor: "",
-                };
-                if (result.numberOfVotes === highestVote) {
-                    shownBarStyle.backgroundColor = "var(--c-primary)";
-                }
-                return (
-                    <div key={i}>
-                        <p>
-                            {result.optionName}, with {result.numberOfVotes} votes
-                        </p>
-                        <div className={styles.tank}>
-                            <div className={styles.liquid} style={shownBarStyle} />
+            <hr id={styles.poll_result_break} />
+            <h3>
+                <b>{totalVotes}</b> answers
+            </h3>
+            <div style={{ width: "100%" }}>
+                {results.map((result, i) => {
+                    const votePercent = Math.round((result.numberOfVotes / totalVotes) * 100) + "%";
+                    let shownBarStyle = {
+                        width: votePercent,
+                        backgroundColor: "",
+                    };
+                    if (result.numberOfVotes === highestVote) {
+                        shownBarStyle.backgroundColor = "var(--c-primary)";
+                    }
+                    return (
+                        <div key={i}>
+                            <p className={styles.bar_label}>
+                                {result.optionName}, with {result.numberOfVotes} votes
+                            </p>
+                            <div className={styles.bar_container}>
+                                <div className={styles.bar_coloured} style={shownBarStyle} />
+                                <span>
+                                    {votePercent} ({result.numberOfVotes})
+                                </span>
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
         </div>
     );
 };
