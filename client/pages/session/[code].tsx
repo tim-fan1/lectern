05595a1336bd309def3d6072da4647f88a070d59
Tim Fan <tfan478@gmail.com>
2021-11-09 15:06:30 +1100

    MCQuiz: this component is the same as poll.

--- client/pages/session/[code].tsx
+++ client/pages/session/[code].tsx
@@ -6,7 +6,7 @@ import Poll from "../../components/Poll";
 import styles from "../../styles/session.module.css";
 import { SessionActivity } from "../../util";
 import NavigationSession from "../../components/NavigationSession";
-
+import MultipleChoiceQuiz from "../../components/MultipleChoiceQuiz";
 const title = "What is the best web development software for complexity?";
 
 function getActivityElement(activity: SessionActivity) {
@@ -23,6 +23,33 @@ function getActivityElement(activity: SessionActivity) {
                     ]}
                 />
             );
+        case SessionActivity.QUIZ:
+            return (
+                <>
+                    <MultipleChoiceQuiz
+                        title={"What is the best web development software for complexity?"}
+                        /* One of these answers is the correct answer. Of course, the client
+                         * won't know which one it is at this point. So really, this is just
+                         * the same as <Poll /> */
+                        answers={[
+                            "Package managers",
+                            "JavaScript bundlers",
+                            "Frameworks on top of frameworks (e.g. Next.js)",
+                            "All of the above",
+                        ]}
+                    />
+                    {/* <DragAndDropQuiz
+                        title={title}
+                        questions={[
+                            "Package managers",
+                            "JavaScript bundlers",
+                            "Frameworks on top of frameworks (e.g. Next.js)",
+                            "All of the above",
+                        ]}
+                    /> */}
+                </>
+            );
+
         default:
             return <p>Coming soon™</p>;
     }
