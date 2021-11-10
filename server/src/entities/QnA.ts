ced504f40a6c662e4efb22cfd801478473c77b21
brian <briannguyen013@gmail.com>
2021-11-11 05:19:38 +1100

    init qna entity

--- server/src/entities/QnA.ts
+++ server/src/entities/QnA.ts
@@ -1,12 +1,50 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import {
+    Column,
+    Entity,
+    ManyToOne,
+    OneToMany,
+    PrimaryGeneratedColumn,
+} from "typeorm";
+
+@ObjectType()
+@Entity()
 class Question {
+    @Field(() => Int)
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Field()
+    @Column()
     question!: string;
+
+    @Field()
+    @Column()
     read!: boolean;
+
+    @Field()
+    @Column()
     authorName!: string;
+
+    @Field(() => QnA)
+    @ManyToOne(() => QnA, (qna) => qna.questions)
+    QnA!: QnA;
 }
 
+@ObjectType()
+@Entity()
 export default class QnA {
+    @Field(() => Int)
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Field(() => Question)
+    @OneToMany(() => Question, (question) => question.QnA, {
+        orphanedRowAction: "delete",
+        cascade: true,
+        nullable: false,
+    })
     questions: Question[] = [];
+    @Field()
     open: boolean;
 }
-
-// TODO: add tgql and orm decorators
