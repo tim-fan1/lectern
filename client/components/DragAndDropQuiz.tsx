19cb2c7c9032dd63ec0537a7e2f278381f60198f
Tim Fan <tfan478@gmail.com>
2021-11-13 13:06:36 +1100

    made it look '''''pretty'''''
    maybe not use inline styling

--- client/components/DragAndDropQuiz.tsx
+++ client/components/DragAndDropQuiz.tsx
@@ -14,66 +14,95 @@ export default function DragAndDropQuiz({ title, answers: _answers }: DragAndDro
     };
     const [answers, setAnswers] = useState(_answers);
     return (
-        <DragDropContext
-            onDragEnd={(result) => {
-                const { destination, source } = result;
-                if (!destination) {
-                    return;
-                }
-                if (
-                    destination.droppableId === source.droppableId &&
-                    destination.index === source.index
-                ) {
-                    return;
-                }
-                const newAnswers = Array.from(answers);
-                /* Remove the element right after source.index. */
-                newAnswers.splice(source.index, 1);
-                /* Place the element at destination.index. */
-                newAnswers.splice(destination.index, 0, answers[source.index]);
-                /* Set answers to be this. */
-                setAnswers(newAnswers);
-            }}
-        >
-            <Droppable droppableId="1">
-                {(provided, snapShot) => (
-                    <div
-                        className={styles.poll_box}
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                    >
-                        <h2>{title}</h2>
-                        <form>
-                            {answers.map((answer, index) => {
-                                return (
-                                    <Draggable
-                                        key={index}
-                                        draggableId={`draggable-${index}`}
-                                        index={index}
-                                    >
-                                        {(provided, snapshot) => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.dragHandleProps}
-                                                {...provided.draggableProps}
+        <>
+            <DragDropContext
+                onDragEnd={(result) => {
+                    const { destination, source } = result;
+                    if (!destination) {
+                        return;
+                    }
+                    if (
+                        destination.droppableId === source.droppableId &&
+                        destination.index === source.index
+                    ) {
+                        return;
+                    }
+                    const newAnswers = Array.from(answers);
+                    /* Remove the element right after source.index. */
+                    newAnswers.splice(source.index, 1);
+                    /* Place the element at destination.index. */
+                    newAnswers.splice(destination.index, 0, answers[source.index]);
+                    /* Set answers to be this. */
+                    setAnswers(newAnswers);
+                }}
+            >
+                <div style={{ display: "flex", justifyContent: "center", margin: "1.5rem" }}>
+                    <h2 style={{ maxWidth: "80%", textAlign: "center" }}>{title}</h2>
+                </div>
+                <Droppable droppableId="1">
+                    {(provided, snapShot) => (
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
+                        >
+                            <div
+                                className={styles.poll_box}
+                                ref={provided.innerRef}
+                                {...provided.droppableProps}
+                            >
+                                <form
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    {answers.map((answer, index) => {
+                                        return (
+                                            <Draggable
+                                                key={index}
+                                                draggableId={`draggable-${index}`}
+                                                index={index}
                                             >
-                                                <h3>{answer}</h3>
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                );
-                            })}
+                                                {(provided, snapshot) => (
+                                                    <div
+                                                        ref={provided.innerRef}
+                                                        {...provided.dragHandleProps}
+                                                        {...provided.draggableProps}
+                                                    >
+                                                        <div
+                                                            style={{
+                                                                margin: "1rem",
+                                                                backgroundColor: "var(--c-text)",
+                                                                color: "black",
+                                                                padding: "1.5rem 3rem",
+                                                                borderRadius: "5px",
+                                                                fontWeight: 400,
+                                                            }}
+                                                        >
+                                                            {answer}
+                                                        </div>
+                                                    </div>
+                                                )}
+                                            </Draggable>
+                                        );
+                                    })}
+                                </form>
+                                {provided.placeholder}
+                            </div>
                             <input
                                 className={"btn btn_call_to_action"}
                                 type={"button"}
                                 value={"Submit"}
                                 onClick={(e) => submitSelectedAnswer()}
                             />
-                        </form>
-                        {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
-        </DragDropContext>
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        </>
     );
 }
